8645267f71409f10851dacdda8cf4e2439a9ee3e
bondi155 <78815412+bondi155@users.noreply.github.com>
2023-08-09 22:39:11 -0600

    api url commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://uleadairsystem.onrender.com'
+export const API_URL = 'https://prueba-ulead.onrender.com'
 //uleadair-production.up.railway.app
 //http://localhost:5015
 //https://prueba-ulead.onrender.com https://uleadairsystem.onrender.com
