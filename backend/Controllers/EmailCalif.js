15487bdcb1dd73af3c295809e2f6dcfad8352142
bondi155 <78815412+bondi155@users.noreply.github.com>
2023-12-28 22:00:51 -0300

    catch try commit

--- backend/Controllers/EmailCalif.js
+++ backend/Controllers/EmailCalif.js
@@ -28,74 +28,72 @@ async function queryforEmail(mes, anio) {
 
 //queryforEmail();
 // Ejemplo de función para enviar un correo electrónico transaccional
-function enviarCorreoTransaccional(result) {
-  result.forEach((persona) => {
+async function enviarCorreoTransaccional(result) {
+  for (const persona of result) {
     if (persona.applicant_area === 'Tripulación') {
-      const emailData = {
-        sender: {
-          name: 'Uleadair: Tu calificación',
-          email: 'uleadair_noreply@calification.com',
-        },
-        to: [
-          {
+      try {
+        const emailData = {
+          sender: {
+            name: 'Uleadair: Tu calificación',
+            email: 'uleadair_noreply@calification.com',
+          },
+          to: [{
             email: persona.company_email,
             name: persona.full_name,
-          },
-        ],
-        subject: 'Calificación de Evaluación Uleadair',
-        htmlContent: `
-      <html>
-          <head>
-              <style>
-                  body { font-family: Arial, sans-serif; }
-                  .container { width: 100%; padding: 20px; }
-                  .header { background-color: #f2f2f2; padding: 10px; text-align: center; }
-                  .content { background-color: #fff; padding: 20px; }
-                  .footer { background-color: #f2f2f2; padding: 10px; text-align: center; }
-                  .passed { color: green; }
-                  .failed { color: red; }
-              </style>
-          </head>
-          <body>
-              <div class="container">
-                  <div class="header">
-                  <img src="https://iili.io/JRM2xbs.png" alt="Logotipo" border="0" />
-                      <h2>Resultados de Evaluación</h2>
+          }],
+          subject: 'Calificación de Evaluación Uleadair',
+          htmlContent: `
+          <html>
+              <head>
+                  <style>
+                      body { font-family: Arial, sans-serif; }
+                      .container { width: 100%; padding: 20px; }
+                      .header { background-color: #f2f2f2; padding: 10px; text-align: center; }
+                      .content { background-color: #fff; padding: 20px; }
+                      .footer { background-color: #f2f2f2; padding: 10px; text-align: center; }
+                      .passed { color: green; }
+                      .failed { color: red; }
+                  </style>
+              </head>
+              <body>
+                  <div class="container">
+                      <div class="header">
+                      <img src="https://iili.io/JRM2xbs.png" alt="Logotipo" border="0" />
+                          <h2>Resultados de Evaluación</h2>
+                      </div>
+                      <div class="content">
+                          <p>Estimado P. A ${persona.full_name},</p>
+                          <p>Le informamos que en su evaluación de Radiocomunicaciones Aeronáuticas en Inglés realizada el <strong>${
+                            persona.first_exam
+                          } a las ${persona.time}</strong></p>
+                          ${
+                            ['A', 'B+', 'B'].includes(persona.exam_calif)
+                              ? '<p class="passed">Usted obtuvo una calificación Aprobatoria.</p>'
+                              : '<p class="failed">Usted obtuvo una calificación Reprobatoria.</p>'
+                          }
+                      </div>
+                      <div class="footer">
+                          <p>Agradecemos su participación y le deseamos un buen día. Si está interesado en cursos de inglés aeronáutico o radiocomunicaciones puede comunicarse por teléfono o whatsapp al 55 5068 3408 con la Lic. Alicia Pérez para obtener información.</p>
+                      </div>
                   </div>
-                  <div class="content">
-                      <p>Estimado P. A ${persona.full_name},</p>
-                      <p>Le informamos que en su evaluación de Radiocomunicaciones Aeronáuticas en Inglés realizada el <strong>${
-                        persona.first_exam
-                      } a las ${persona.time}</strong></p>
-                      ${
-                        ['A', 'B+', 'B'].includes(persona.exam_calif)
-                          ? '<p class="passed">Usted obtuvo una calificación Aprobatoria.</p>'
-                          : '<p class="failed">Usted obtuvo una calificación Reprobatoria.</p>'
-                      }
-                  </div>
-                  <div class="footer">
-                      <p>Agradecemos su participación y le deseamos un buen día. Si está interesado en cursos de inglés aeronáutico o radiocomunicaciones puede comunicarse por teléfono o whatsapp al 55 5068 3408 con la Lic. Alicia Pérez para obtener información.</p>
-                  </div>
-              </div>
-          </body>
-      </html>`,
-      };
+              </body>
+          </html>`
+        };
 
-      apiInstance.sendTransacEmail(emailData).then(
-        function () {
-          console.log(`Correos enviados exitosamente para Tripulación ${persona.company_email} del mes de ${persona.month}`);
-        },
-        function (error) {
-          console.error(
-            'Error al enviar correo a ' + persona.company_email + ': ' + error
-          );
+        await apiInstance.sendTransacEmail(emailData);
+        console.log(`Correo enviado exitosamente a ${persona.company_email}`);
+      } catch (error) {
+        console.error(`Error al enviar correo a ${persona.company_email}:`, error.message);
+        if (error.response) {
+          console.error('Detalles de la respuesta:', error.response.data);
         }
-      );
+      }
     } else {
-      console.log('No se envía correo a ' + persona.company_email + ' debido a que el área no es Tripulación.');
+      console.log(`No se envía correo a ${persona.company_email} debido a que el área no es Tripulación.`);
     }
-  });
+  }
 }
+
 
 async function EmailFunctions(mes, anio) {
   try {
