cfe70f6363db6c87da2b6c68ee141758e4e3a71e
bondi155 <78815412+bondi155@users.noreply.github.com>
2024-05-14 10:36:06 -0600

    Update Config

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:5022'
+export const API_URL = 'https://form-pdf-back.onrender.com'
 //uleadair-production.up.railway.app
-//http://localhost:5015
-//https://prueba-ulead.onrender.com https://uleadairsystem.onrender.com
+//http://localhost:5022
+//https://prueba-ulead.onrender.com https://uleadairsystem.onrender.com https://form-pdf-back.onrender.com
